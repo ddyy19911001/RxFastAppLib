06d4dd3f9349357148c3203e6da60c4c1149d910
dengyin <584120792@qq.com>
2020-08-05 17:41:15 +0800

    去除了页面切换动画，可自由在manifast中设置style，优化了请求数据的打印。增加转圈button，增加文字资源国际化，修改弹框。

--- fastframework/src/main/java/com/dy/fastframework/activity/BaseActivity.java
+++ fastframework/src/main/java/com/dy/fastframework/activity/BaseActivity.java
@@ -1,12 +1,20 @@
 package com.dy.fastframework.activity;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+import android.view.WindowManager;
+
 import com.dy.fastframework.R;
+import com.dy.fastframework.view.CommonMsgDialog;
 import com.vise.xsnow.http.ViseHttp;
 import com.vise.xsnow.http.config.HttpGlobalConfig;
 
 import yin.deng.superbase.activity.SuperBaseActivity;
 
 public abstract class BaseActivity extends SuperBaseActivity {
+    private CommonMsgDialog commonMsgDialog;
+
     @Override
     public void onNotInitFirst() {
         ViseHttp.CONFIG().setOnRequestWatingDialogListener(new HttpGlobalConfig.OnRequestWatingDialogListener() {
@@ -21,5 +29,41 @@ public abstract class BaseActivity extends SuperBaseActivity {
             }
         });
         super.onNotInitFirst();
+    }
+
+    public void showMsgDialog(String msg){
+        commonMsgDialog=getCommonMsgDialog();
+        commonMsgDialog.showMsg(msg);
+    }
+
+    private CommonMsgDialog getCommonMsgDialog() {
+        if(commonMsgDialog==null) {
+            commonMsgDialog = new CommonMsgDialog(this);
+        }
+        return commonMsgDialog;
+    }
+
+
+    /**
+     *
+     * @param statubarBackGroundColor  状态栏背景色
+     * @param isStatusTextWhite  状态栏文字是否为白色
+     */
+    public void changeStatustBarColor(int statubarBackGroundColor,boolean isStatusTextWhite){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // 设置状态栏底色白色
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().setStatusBarColor(statubarBackGroundColor);
+            // 设置状态栏字体颜色
+            if(isStatusTextWhite){
+                //白色
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            }else{
+                //黑色
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+
+        }
     }
 }
--- fastframework/src/main/java/com/dy/fastframework/fragment/BaseFragment.java
+++ fastframework/src/main/java/com/dy/fastframework/fragment/BaseFragment.java
@@ -1,6 +1,22 @@
 package com.dy.fastframework.fragment;
 
+import com.dy.fastframework.view.CommonMsgDialog;
+
 import yin.deng.superbase.fragment.ViewPagerSuperBaseFragment;
 
 public abstract class BaseFragment extends ViewPagerSuperBaseFragment {
+    private CommonMsgDialog commonMsgDialog;
+
+    public void showMsgDialog(String msg){
+        commonMsgDialog=getCommonMsgDialog();
+        commonMsgDialog.showMsg(msg);
+    }
+
+    private CommonMsgDialog getCommonMsgDialog() {
+        if(commonMsgDialog==null) {
+            commonMsgDialog = new CommonMsgDialog(getActivity());
+        }
+        return commonMsgDialog;
+    }
+
 }
